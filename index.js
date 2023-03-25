bb3151add325d6b07b45694b10d1d0b9d29f8b27
Wonxen <85366208+Wonxen@users.noreply.github.com>
2023-03-25 12:09:52 +0300

    Delete index.js

--- index.js
+++
@@ -1,2 +0,0 @@
-Bot hala geliştirilmekte olup geliştirilmeye devam edilecektir tamamen gelişip sorunsuz hale geldiğinde paylaşılacaktır.
-Bot geliştirildikce görseller yüklenecektir 100 Star 25 Fork da paylaşılacaktır.
